92496b1004ea0c127aedbb57953e06454bddf362
Barry A Dobyns <barry@productops.com>
2016-01-18 10:00:59 -0800

    BOLT-1611 adding more code for self-signed certificates in case the letsencrypt stuff fails, as it turns out to have done

--- deploy-looker/content/home/looker/scripts/postinst-part2.sh
+++ deploy-looker/content/home/looker/scripts/postinst-part2.sh
@@ -34,6 +34,8 @@ ME=`hostname -f`
 SSL=$LOOKERHOME/ssl
 mkdir -p $SSL
 LE=/etc/letsencrypt/archive/$ME
+mkdir -p $LE
+PASSWORD=looker
 # check to see if this particular cert is already present.  it may be.
 if [ ! -f $LE/cert1.pem ] ; then
     ./letsencrypt-auto certonly --webroot -w /usr/share/nginx/html -d $ME --email barry@productops.com --agree-tos
@@ -48,10 +50,17 @@ for F in  $LE/cert1.pem $LE/fullchain1.pem $LE/privkey1.pem
 do
     if [ ! -f $F ] 
     then 
-	echo "ERROR: no $F"
-	echo  '      maybe this command failed mysteriously?'
-	echo ./letsencrypt-auto certonly --webroot -w /usr/share/nginx/html -d $ME --email barry@productops.com --agree-tos
-	exit 42
+	# generate a self-signed certificate
+	openssl req -x509 -newkey rsa:2048 \
+	    -keyout $LE/privkey1.pem \
+	    -out $LE/cert1.pem \
+	    -passout pass:$PASSWORD \
+	    -days 3650 \
+	    -subj "/C=US/ST=Michigan/L=AnnArbor/O=Ithaka.org/CN=$ME"
+#	echo "ERROR: no $F"
+#	echo  '      maybe this command failed mysteriously?'
+#	echo ./letsencrypt-auto certonly --webroot -w /usr/share/nginx/html -d $ME --email barry@productops.com --agree-tos
+#	exit 42
     fi
 done
 
@@ -60,19 +69,31 @@ chown -R www-data:www-data /etc/letsencrypt
 find /etc/letsencrypt -type d -exec chmod ugo+rx "{}" ";"
 
 # LOOKER.JAR NEEDS A JAVA KEYSTORE --------------------------------------------
-echo looker >$SSL/keystorepass
+# store the magic password in a file
+echo $PASSWORD >$SSL/keystorepass
+# the $SSL/3pass file just keeps the keytool from hanging on input
+( cat $SSL/keystorepass ;cat $SSL/keystorepass ;cat $SSL/keystorepass ) >$SSL/3pass
 # now following http://www.looker.com/docs/setup-and-management/on-prem-install/ssl-setup
 rm -f $SSL/looker.p12
+# if we used letsencrypt, there's no password for the privkey1
+if [ -f $LE/fullchain1.pem ] ; then 
 openssl pkcs12 -export \
   -in $LE/cert1.pem \
-  -CAfile $LE/fullchain1.pem \
+  -CAfile $LE/fillchain1.pem \  
   -inkey $LE/privkey1.pem \
   -out $SSL/looker.p12 \
   -passin pass: -passout file:$SSL/keystorepass
+else
+# we used a self-signed certificate, but there IS a password, and no chain
+openssl pkcs12 -export \
+  -in $LE/cert1.pem \
+  -inkey $LE/privkey1.pem \
+  -out $SSL/looker.p12 \
+  -passin pass:$PASSWORD -passout file:$SSL/keystorepass
+fi
 
-# the $SSL/3pass file just keeps the keytool from hanging on input
+# this creates a java keystore, which is what looker ultimately needs
 rm -f $SSL/looker.jks
-( cat $SSL/keystorepass ;cat $SSL/keystorepass ;cat $SSL/keystorepass ) >$SSL/3pass
 cat $SSL/3pass | keytool -importkeystore \
   -srckeystore $SSL/looker.p12 \
   -srcstoretype pkcs12 \
@@ -99,11 +120,14 @@ NGC=/etc/nginx/nginx.conf
 if [ ! -f ${NGC}.orig ] ; then 
     cp $NGC ${NGC}.orig
 fi
+USNH=/usr/share/nginx/html
 
 # change the domain name, and then point to the keys we made earlier 
 cat $LOOKERHOME/nginx/looker.conf | sed -e s/looker.domain.com/$ME/ \
     -e "s+/etc/looker/ssl/certs/self-ssl.crt+/etc/letsencrypt/archive/$ME/cert1.pem+" \
     -e "s+/etc/looker/ssl/private/self-ssl.key+/etc/letsencrypt/archive/$ME/privkey1.pem+"  >$NGC
+# rewrite the index.html so that it redirects to the correct ssl page
+cat $LOOKERHOME/nginx/index.html | sed -e s/looker.domain.com/$ME/g >$USNH/index.html
 
 if ! service nginx restart
 then
