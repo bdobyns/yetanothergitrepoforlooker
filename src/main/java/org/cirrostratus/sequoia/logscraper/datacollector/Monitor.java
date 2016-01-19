5eff39d81f6ca841025e3b04293a5d7f84241ed0
Will Bartee <will.bartee@productops.com>
2016-01-18 23:53:18 -0800

    Modified pom

--- src/main/java/org/cirrostratus/sequoia/logscraper/datacollector/Monitor.java
+++ src/main/java/org/cirrostratus/sequoia/logscraper/datacollector/Monitor.java
@@ -1,6 +1,5 @@
 package org.cirrostratus.sequoia.logscraper.datacollector;
 
-import com.wordnik.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +13,6 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/")
-@Api(value="Base Monitor Class")
 public class Monitor {
 
     @Autowired
